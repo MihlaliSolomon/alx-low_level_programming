857d9d88c11b05887967cdb1470d3cc6af53e2e1
Mihlali <mihlalisolomon@gmail.com>
2022-06-02 18:37:08 -0700

    Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
-#include <stdio.h>
+#include <iostream.h>
 
 /**
  * main - Entry point
  *
  * Return: Always 1 (Success)
  */
-int main(void)
+void main ()
 {
-	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	cout << "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	return (1)
 }
