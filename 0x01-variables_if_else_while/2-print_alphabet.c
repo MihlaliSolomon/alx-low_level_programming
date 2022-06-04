59f5f7534101b1f43c64cc2dbef0502c597d300e
Mihlali <mihlalisolomon@gmail.com>
2022-06-03 19:00:00 -0700

    Print Alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,7 +6,7 @@
  *
  * Return: Always 0 (Success)
  */
-int main()
+int main(void)
 {
 	char alpha = 'a';
 
@@ -16,5 +16,5 @@ int main()
 		alpha++;
 	}
 	putchar('\n');
-	return (0)
+	return (0);
 }
