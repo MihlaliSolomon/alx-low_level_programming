de123a1395d4c9d55e37fa32777d84baeafc5bbc
Mihlali <mihlalisolomon@gmail.com>
2022-06-03 19:34:03 -0700

    whitespace

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,19 +10,19 @@ int main(void)
 {
 	char lowerc = 'a';
 	char upperc = 'A';
-	
+
 	while (lowerc <= 'z')
 	{
 		putchar(lowerc);
 		lowerc++;
 	}
-	
+
 	while (upperc <= 'Z')
 	{
 		putchar(upperc);
 		upperc++;
 	}
-	
+
 	putchar('\n');
 
 	return (0);
