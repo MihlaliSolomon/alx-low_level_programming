2d22b9d1fe387cc839ea831c38bcdd12d8839daa
Mihlali <mihlalisolomon@gmail.com>
2022-06-07 10:58:26 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "_putchar"
+
+/**
+ * main - Prints _putchar
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char *ms = "_putchar";
+
+	while (*ms)
+	{
+		_putchar(*ms);
+		ms++;
+	}
+	_putchar('\n');
+
+	return (0);
+
+}
